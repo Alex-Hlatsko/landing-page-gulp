c0e0c1247c841b0cb9c47bd62e28e6a2867e69b5
Alen-Berg <alexei.hlatsko@gmail.com>
2023-02-10 20:07:52 +0100

    contact

--- src/js/index.js
+++ src/js/index.js
@@ -2,3 +2,19 @@ const openMenu = () => {
    document.getElementById("burger-menu").classList.toggle("active");
    document.getElementById("header").classList.toggle("active");
 }
+
+const closeMenu = () => {
+   if(document.getElementById("header").classList.contains('active')){
+      document.getElementById("burger-menu").classList.toggle("active");
+      document.getElementById("header").classList.toggle("active");
+   }
+}
+
+$('.single-item').slick({
+   arrows: true,
+   prevArrow:"<button type='button' class='slick-prev'><<</button>",
+   nextArrow:"<button type='button' class='slick-next'>>></button>",
+   dots: false,
+   slidesToShow: 1,
+   slidesToScroll: 1,
+});
